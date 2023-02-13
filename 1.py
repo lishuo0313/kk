ce53bc04c67a485bb7f98f5fe017821e8947dd78
lishuo0313 <125233289+lishuo0313@users.noreply.github.com>
2023-02-13 14:07:31 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ openai.api_key = "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"
 robot = werobot.WeRoBot()
 
 class RobotConfig(object):
-    HOST="0.0.0.0"
+    HOST="10.9.100.246"
     PORT= 80
     TOKEN = "123456"# token是微信公众号用来指定接入当前云服务器的服务的凭证，代表是自己人接入的，等一下就有什么用了
     
